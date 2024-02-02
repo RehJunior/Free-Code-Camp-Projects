a8eb7aa2085a196678fce4aa8c654b58c25b6665
RehJunior <samet.30101468@gmail.com>
2024-02-02 10:23:29 +0100

    new addings

--- Music Player/script.js
+++ Music Player/script.js
@@ -264,5 +264,16 @@ audio.addEventListener("ended", () => {
   }
 });
 
+userData?.songs.sort((a, b) => {
+  if (a.title < b.title) {
+    return -1;
+  }
+
+  if (a.title > b.title) {
+    return 1;
+  }
+
+  return 0;
+});
 renderSongs(userData?.songs);
 setPlayButtonAccessibleText();
